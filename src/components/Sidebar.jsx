a6b8b31f9781e77519a6cffef7fff2b8b6c399b0
sebaei <47517850+sebaei@users.noreply.github.com>
2022-12-30 23:14:25 +0200

    Add Discover page and Song Cards

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import { HiOutlineMenu } from "react-icons/hi";
+
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
+const NavLinks = (handleClick) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+        ) : (
+          <HiOutlineMenu className="w-6 h-6 text-white mr-2" />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
